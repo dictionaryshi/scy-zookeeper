4ed6e0878a4289213ea8f41836219962a0bccae6
史春阳 <shichunyang02@meituan.com>
2021-07-07 16:52:16 +0800

    ConfigCenter.java

---
+++ src/main/java/com/scy/zookeeper/annotation/ConfigCenter.java
@@ -0,0 +1,20 @@
+package com.scy.zookeeper.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author : shichunyang
+ * Date    : 2021/7/7
+ * Time    : 4:44 下午
+ * ---------------------------------------
+ * Desc    : 配置中心
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConfigCenter {
+
+    String key();
+}

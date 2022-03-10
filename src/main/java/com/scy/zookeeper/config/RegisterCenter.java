1509fcc490e838666c6cdc00dcca818540aef4c0
史春阳 <shichunyang02@meituan.com>
2022-03-10 13:25:26 +0800

    RegisterCenter.java

--- src/main/java/com/scy/zookeeper/config/RegisterCenter.java
+++ src/main/java/com/scy/zookeeper/config/RegisterCenter.java
@@ -1,13 +1,19 @@
 package com.scy.zookeeper.config;
 
+import com.scy.core.ArrayUtil;
+import com.scy.core.CollectionUtil;
+import com.scy.core.ObjectUtil;
+import com.scy.core.StringUtil;
 import com.scy.core.thread.ThreadPoolUtil;
 import com.scy.zookeeper.ZkClient;
 import com.scy.zookeeper.listener.CuratorListener;
 import com.scy.zookeeper.listener.DataListener;
+import com.scy.zookeeper.model.RegisterCenterData;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -47,21 +53,62 @@ public class RegisterCenter {
         return envPath + "/" + serviceKey;
     }
 
+    private RegisterCenterData getRegisterCenterData(String path) {
+        if (StringUtil.isEmpty(path)) {
+            return null;
+        }
+
+        if (path.length() <= envPath.length()) {
+            return null;
+        }
+
+        String[] dataArr = path.substring(envPath.length() + 1).split("/");
+        if (ArrayUtil.isEmpty(dataArr) || ArrayUtil.getLength(dataArr) < 2) {
+            return null;
+        }
+
+        RegisterCenterData registerCenterData = new RegisterCenterData();
+        registerCenterData.setServiceKey(dataArr[0]);
+        registerCenterData.setAddress(dataArr[1]);
+        return registerCenterData;
+    }
+
     public void init() {
         CuratorListener curatorListener = new CuratorListener(zkClient, new DataListener() {
 
             @Override
             public void add(String path, String data) {
+                listener(path);
             }
 
             @Override
             public void update(String path, String oldData, String newData) {
+                listener(path);
             }
 
             @Override
             public void delete(String path, String data) {
+                listener(path);
             }
         });
         zkClient.addListener(envPath, curatorListener, ThreadPoolUtil.getThreadPool("registerCenter-pool", 10, 10, 1024));
+    }
+
+    private void listener(String path) {
+        RegisterCenterData registerCenterData = getRegisterCenterData(path);
+        if (ObjectUtil.isNull(registerCenterData)) {
+            return;
+        }
+
+        String servicePath = serviceKeyToPath(registerCenterData.getServiceKey());
+        List<String> addresses = zkClient.getChildren(servicePath);
+        if (CollectionUtil.isEmpty(addresses)) {
+            return;
+        }
+
+        addresses.forEach(address -> {
+            String addressPath = servicePath + "/" + address;
+            String addressData = zkClient.doGetContent(addressPath);
+        });
     }
 }
